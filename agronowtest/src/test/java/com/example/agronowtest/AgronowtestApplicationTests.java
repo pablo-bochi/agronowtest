eea03ea1bfdaa8da75f80b055f882022b9805a56
pablo-bochi <pablovilelabochi@gmail.com>
2020-01-13 12:43:10 -0200

    Primeiro Commit.

---
+++ agronowtest/src/test/java/com/example/agronowtest/AgronowtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agronowtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgronowtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
